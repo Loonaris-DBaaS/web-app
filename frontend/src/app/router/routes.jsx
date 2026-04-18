38259c29bb4347ac61bf1551f004a531056319d8
HIJOdelIDANII <ahmed.idani@insat.ucar.tn>
2026-04-18 23:39:42 +0100

    (fix) fixing import typo and adding vite to dev dependencies

--- frontend/src/app/router/routes.jsx
+++ frontend/src/app/router/routes.jsx
@@ -1,18 +1,18 @@
-import Landing from'../../pages/Landing/Landing';
-import AuthLayout from '../../layouts/AuthLayout';
-import SignIn from '../../pages/SignIn/SignIn';
-import SignUp from '../../pages/SignUp/SignUp';
+import Landing from "../../pages/Landing/Landing";
+import AuthLayout from "../../layouts/AuthLayout";
+import SignIn from "../../pages/SignIn/Signin";
+import SignUp from "../../pages/SignUp/Signup";
 const routes = [
-    {
-        path:'/',
-        element:<Landing/>
-    },
-    {
-        element : <AuthLayout/>,
-        children: [
-            { path: '/signin', element: <SignIn /> },
-            { path: '/signup', element: <SignUp /> },
-        ],
-    }
-]
-export default routes
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      { path: "/signin", element: <SignIn /> },
+      { path: "/signup", element: <SignUp /> },
+    ],
+  },
+];
+export default routes;
