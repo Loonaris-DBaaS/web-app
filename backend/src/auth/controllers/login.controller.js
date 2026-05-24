8e0aa2f93121f9374e409a5f22580249a9eead89
Mariem Abida <mariem.abida@insat.ucar.tn>
2026-05-24 12:30:54 +0100

    added prisma schema and config

---
+++ backend/src/auth/controllers/login.controller.js
@@ -0,0 +1,6 @@
+async function login(req, res) {
+    const {name,email,password} = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: 'Name, email and password are required' });
+    }
+}
